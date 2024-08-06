d22079d24dfbed11250e3266f7e07cfd3ba70ae1
shushinde111@gmail.com <shushinde111@gmail.com>
2024-08-06 13:07:56 +0530

    cron at 10 13 6 8 *

--- controllers/admin/leaveAddController.js
+++ controllers/admin/leaveAddController.js
@@ -251,7 +251,7 @@ const viewHolidays = async (req, res) => {
   }
 };
 
-cron.schedule("3 13 6 8 * *", () => {
+cron.schedule("10 13 6 8 *", () => {
   addLeaves();
   console.log("Scheduled addLeaves task executed on the 1st day of the month.");
 });
