86cbcea8a106c2bb3950deda6b5ce23b8ee32690
shushinde111@gmail.com <shushinde111@gmail.com>
2024-08-07 14:43:03 +0530

    cron updated

--- Jobs/start.js
+++ Jobs/start.js
@@ -4,7 +4,7 @@ const {
 } = require("../controllers/admin/leaveAddController");
 
 const startJobs = () => {
-  //   jobAddLeaves.start();
+  jobAddLeaves.start();
   jobUpdateOptional.start();
 };
 
